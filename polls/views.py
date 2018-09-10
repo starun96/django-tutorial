6940c20319fdc281333e8d44edf77bb7b92abe45
starun96 <tarunsaharya@gmail.com>
2018-09-09 22:03:25 -0400

    changed the functions into generic views

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
+from django.utils import timezone
 # from django.template import loader
 from .models import Question
 # Create your views here.
