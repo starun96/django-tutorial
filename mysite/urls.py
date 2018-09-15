ff26f4ca96e8720d36cddac00802bbbfb3357f31
starun96 <tarunsaharya@gmail.com>
2018-09-15 15:16:17 -0400

    added new urls for blog stuff

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url, include
+from blog.views import post_model_list_view
+
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    url(r'^blog/', include(('blog.urls', 'blog'), namespace='blog')),
 
 ]
