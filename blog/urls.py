7216bd7f0a1d14603b0fb42d642ae8db8257f897
starun96 <tarunsaharya@gmail.com>
2018-09-15 15:16:36 -0400

    added the blog app dir

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from .views import (post_model_list_view,   post_model_create_view,
+                    post_model_detail_view, post_model_update_view, post_model_delete_view)
+
+
+urlpatterns = [
+    url(r'^$', post_model_list_view, name='list'),
+    url(r'^create/$', post_model_create_view, name='create'),
+    url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
+    url(r'^(?P<id>\d+)/update$', post_model_update_view, name='update'),
+    url(r'^(?P<id>\d+)/delete$', post_model_delete_view, name='delete'),
+]
---
+++ blog/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import PostModel
+from .forms import PostModelForm
+from django.shortcuts import get_object_or_404
+
+
+def post_model_list_view(request):
+    qs = PostModel.objects.all()
+
+    template = "blog/list-view.html"
+    context = {
+        "object_list": qs,
+    }
+    return render(request, template, context)
+
+
+def post_model_create_view(request):
+    template = "blog/create-view.html"
+    form = PostModelForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        context = {
+            "form": PostModelForm()
+        }
+    return render(request, template, context)
+
+
+def post_model_detail_view(request, id=None):
+    obj = get_object_or_404(PostModel, id=id)
+    context = {"object": obj, }
+    template = "blog/detail-view.html"
+    return render(request, template, context)
+
+
+def post_model_update_view(request, id=None):
+    template = "blog/update-view.html"
+    obj = get_object_or_404(PostModel, id=id)
+
+    form = PostModelForm(request.POST or None, instance=obj)
+    context = {
+        "form": form,
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        context = {
+            "form": PostModelForm()
+        }
+
+    return render(request, template, context)
+
+
+def post_model_delete_view(request, id=None):
+    template = "blog/delete-view.html"
+    obj = get_object_or_404(PostModel, id=id)
+    context = {
+        "object": obj,
+    }
+
+    return render(request, template, context)
