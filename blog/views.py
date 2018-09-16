51350f4ade35a63ae3d32bf0cac8738381396036
starun96 <tarunsaharya@gmail.com>
2018-09-16 19:37:05 -0400

    added update view, which updates a model after changes

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
+from django.shortcuts import get_object_or_404
 from .models import PostModel
 from .forms import PostModelForm
-from django.shortcuts import get_object_or_404
 
 
 def post_model_list_view(request):
@@ -31,28 +32,30 @@ def post_model_create_view(request):
     return render(request, template, context)
 
 
+def post_model_update_view(request, id=None):
+    template = "blog/update-view.html"
+    if request.POST:
+        post = get_object_or_404(PostModel, id=id)
+        form = PostModelForm(request.POST or None, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blog:detail', args=(id)))
+    else:
+        post = get_object_or_404(PostModel, id=id)
+        form = PostModelForm(instance=post)
+
+    context = {
+        "form": form,
+        "post_id": id,
+    }
+
+    return render(request, template, context)
+
+
 def post_model_detail_view(request, id=None):
     obj = get_object_or_404(PostModel, id=id)
     context = {"object": obj, }
     template = "blog/detail-view.html"
-    return render(request, template, context)
-
-
-def post_model_update_view(request, id=None):
-    template = "blog/update-view.html"
-    obj = get_object_or_404(PostModel, id=id)
-
-    form = PostModelForm(request.POST or None, instance=obj)
-    context = {
-        "form": form,
-    }
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.save()
-        context = {
-            "form": PostModelForm()
-        }
-
     return render(request, template, context)
 
 
